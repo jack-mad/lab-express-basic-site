e20c91f3db35f40f8a043079e4618ca1ee556bd6
Joaquin Velazquez <joaquin.ve@outlook.com>
2022-01-31 18:01:46 -0600

    index , about y work

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,18 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
+router.get("/work", (req, res) => {
+  res.render("work");
+});
+
+router.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
+
+
 //exportaciones
 module.exports = router;
